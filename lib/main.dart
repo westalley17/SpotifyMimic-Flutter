5aa080d778102bca38c51d3c2e1d79414c0dc36d
Wes Talley <wesatalley@gmail.com>
2024-07-26 19:55:36 -0500

    Added scrollabililty (word? word.)

--- lib/main.dart
+++ lib/main.dart
@@ -56,90 +56,92 @@ class _MyHomePageState extends State<MyHomePage> {
             colors: [Colors.black, Color.fromARGB(255, 47, 45, 45)],
           ),
         ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                margin: const EdgeInsets.fromLTRB(0, 20, 0, 50.0),
-                child: const Image(
-                  width: 150.0,
-                  height: 150.0,
-                  image: AssetImage('assets/Spotify-Logo.png'),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(0, 0, 0, 40.0),
-                child: const Text(
-                  "Music for everyone",
-                  style: TextStyle(color: Colors.white, fontSize: 30.0),
-                ),
-              ),
-              Container(
-                width: 400.0,
-                padding: const EdgeInsets.fromLTRB(30.0, 0, 30.0, 0),
-                margin: const EdgeInsets.fromLTRB(0, 0, 0, 20.0),
-                child: TextField(
-                  controller: emailController,
-                  style: const TextStyle(color: Colors.white),
-                  decoration: const InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.zero),
-                      borderSide: BorderSide(color: Colors.green),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.zero),
-                    ),
-                    hintText: "johndoe@gmail.com",
-                    label: Text("Email"),
-                    floatingLabelBehavior: FloatingLabelBehavior.always,
-                    hintStyle: TextStyle(
-                      color: Color.fromARGB(44, 255, 255, 255),
-                    ),
+        child: SizedBox.expand(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.fromLTRB(0, 20, 0, 50.0),
+                  child: const Image(
+                    width: 150.0,
+                    height: 150.0,
+                    image: AssetImage('assets/Spotify-Logo.png'),
                   ),
                 ),
-              ),
-              Container(
-                width: 400.0,
-                padding: const EdgeInsets.fromLTRB(30.0, 0, 30.0, 0),
-                child: TextField(
-                  controller: passwordController,
-                  style: const TextStyle(color: Colors.white),
-                  decoration: const InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.zero),
-                      borderSide: BorderSide(color: Colors.green),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.zero),
-                    ),
-                    hintText: "Johndoe1234!",
-                    label: Text("Password"),
-                    floatingLabelBehavior: FloatingLabelBehavior.always,
-                    hintStyle: TextStyle(
-                      color: Color.fromARGB(44, 255, 255, 255),
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(0, 30.0, 0, 0),
-                child: FilledButton(
-                  style: const ButtonStyle(
-                    backgroundColor: WidgetStatePropertyAll(Colors.green),
-                  ),
-                  onPressed: () => {},
+                Container(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 40.0),
                   child: const Text(
-                    "Log in",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16.0,
-                      color: Colors.white,
+                    "Music for everyone",
+                    style: TextStyle(color: Colors.white, fontSize: 30.0),
+                  ),
+                ),
+                Container(
+                  width: 400.0,
+                  padding: const EdgeInsets.fromLTRB(30.0, 0, 30.0, 0),
+                  margin: const EdgeInsets.fromLTRB(0, 0, 0, 20.0),
+                  child: TextField(
+                    controller: emailController,
+                    style: const TextStyle(color: Colors.white),
+                    decoration: const InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.zero),
+                        borderSide: BorderSide(color: Colors.green),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.zero),
+                      ),
+                      hintText: "johndoe@gmail.com",
+                      label: Text("Email"),
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                      hintStyle: TextStyle(
+                        color: Color.fromARGB(44, 255, 255, 255),
+                      ),
                     ),
                   ),
                 ),
-              )
-            ],
+                Container(
+                  width: 400.0,
+                  padding: const EdgeInsets.fromLTRB(30.0, 0, 30.0, 0),
+                  child: TextField(
+                    controller: passwordController,
+                    style: const TextStyle(color: Colors.white),
+                    decoration: const InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.zero),
+                        borderSide: BorderSide(color: Colors.green),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.zero),
+                      ),
+                      hintText: "Johndoe1234!",
+                      label: Text("Password"),
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                      hintStyle: TextStyle(
+                        color: Color.fromARGB(44, 255, 255, 255),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(0, 30.0, 0, 0),
+                  child: FilledButton(
+                    style: const ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(Colors.green),
+                    ),
+                    onPressed: () => {},
+                    child: const Text(
+                      "Log in",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
